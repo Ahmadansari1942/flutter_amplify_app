c0f1ef21cfee8ba84ce641c9d9f38de53126ebcd
Ahmadansari1942 <sn0259152@gmail.com>
2026-03-18 13:32:35 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -193,4 +193,15 @@ class StatBox extends StatelessWidget {
     );
   }
 }
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+Future<List<dynamic>> fetchPosts() async {
+  final response = await http.get(Uri.parse("http://13.233.143.117:3000/posts"));
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to load posts');
+  }
+}
